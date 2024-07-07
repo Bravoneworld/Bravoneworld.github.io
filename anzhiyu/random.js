2cd6ebb19465cccb139f4a8980a5ae149a41a137
bravoneworld <bravoneworld@163.com>
2024-07-07 09:34:37 +0800

    Site updated: 2024-07-07 09:34:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/02/Bravoneworld/","2024/07/02/hello-world/"];function toRandomPost(){
+var posts=["2024/07/02/Bravoneworld/","2024/07/02/hello-world/","2024/07/07/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
